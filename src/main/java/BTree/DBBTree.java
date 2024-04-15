cbf7be4134797d72f312ab3b9a6d2b6e326aaca1
Ahmed Gado <ahmedgado0100@gmail.com>
2024-04-15 20:22:23 +0200

    feat: override methods for `DBBTree` class

--- src/main/java/BTree/DBBTree.java
+++ src/main/java/BTree/DBBTree.java
@@ -13,14 +13,62 @@ import java.util.LinkedList;
 
 public class DBBTree<TKey extends Comparable<TKey>> extends BTree<TKey, LinkedList<Integer>> implements Serializable {
 
+    private final String tableName;
+    private final String indexName;
+
+    public DBBTree(String tableName, String indexName) {
+        super();
+        this.tableName = tableName;
+        this.indexName = indexName;
+    }
+
+    @Override
     public LinkedList<Integer> search(TKey key) {
-        return this.search(key);
+        return super.search(key);
+    }
+
+    public void insert(TKey key, Integer value) {
+        LinkedList<Integer> values = this.search(key);
+        if (values == null) {
+            values = new LinkedList<>();
+        }
+        values.add(value);
+        super.insert(key, values);
+
+        this.saveIndex();
+    }
+
+    public void delete(TKey key, Integer value) {
+        LinkedList<Integer> values = this.search(key);
+        if (values != null) {
+            values.remove(value);
+            if (values.isEmpty()) {
+                super.delete(key);
+            } else {
+                super.insert(key, values);
+            }
+        }
+
+        this.saveIndex();
+    }
+
+    // save the bplustree index to the disk
+    public void saveIndex() {
+        Path file = Paths.get((String) DBApp.getDbConfig().get("DataPath"), tableName, indexName + ".ser");
+
+        try (
+                FileInputStream fileIn = new FileInputStream(file.toAbsolutePath().toString());
+                ObjectInputStream in = new ObjectInputStream(fileIn)) {
+            in.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     // fetch the bplustree index from the disk
     public static DBBTree loadIndex(String TableName, String IndexName) throws DBAppException {
         DBBTree tree;
-        Path file = Paths.get(DBApp.getDbConfig().get("DataPath") + "/" + TableName + "/" + IndexName + ".ser");
+        Path file = Paths.get((String) DBApp.getDbConfig().get("DataPath"), TableName, IndexName + ".ser");
 
         if (!file.toFile().exists()) {
             throw new DBAppException("Index file does not exist");
